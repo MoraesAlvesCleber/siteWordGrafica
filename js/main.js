697ae328f97cc044016ebd1c41f01ffe8fdd64dc
Cleber Moraes <alvesmoraesb@gmail.com>
2024-02-13 00:44:03 -0300

    copiando pastas
    css, img, e js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+ 
+    $(document).ready(function(){
+        $("#banner").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 2000, // Tempo em milissegundos
+            autoplayHoverPause: true,
+            dots: false,
+            nav: false
+        })
+    })
+
+    
+ 
